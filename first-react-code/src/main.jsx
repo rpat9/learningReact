678e3fc0267a6cca8945d33f5b8a91c5c61f5c76
rpat9 <rudrap26176@gmail.com>
2025-03-27 14:35:24 -0500

    First-React-Setup

---
+++ first-react-code/src/main.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+
+createRoot(document.getElementById('root')).render(
+  // StrictMode is a tool for highlighting potential problems in an application
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
