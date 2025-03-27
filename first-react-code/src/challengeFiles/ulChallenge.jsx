3589238a5610b94e3fd6f1a0f19ef4bf5783936a
rpat9 <rudrap26176@gmail.com>
2025-03-27 15:09:45 -0500

    First Challenge

---
+++ first-react-code/src/challengeFiles/ulChallenge.jsx
@@ -0,0 +1,15 @@
+function UlChallenge() {
+
+    // Hardcode an unorderd list
+
+    return (
+        <ul>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+        </ul>
+    )
+
+}
+
+export default UlChallenge;
--- first-react-code/src/main.jsx
+++ first-react-code/src/main.jsx
@@ -2,10 +2,19 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
+import Ulchallenge from './challengeFiles/ulChallenge.jsx';
 
 createRoot(document.getElementById('root')).render(
   // StrictMode is a tool for highlighting potential problems in an application
   <StrictMode>
     <App />
   </StrictMode>
+);
+
+createRoot(document.getElementById('ulChallenge')).render(
+  
+  <StrictMode>
+    <Ulchallenge />
+  </StrictMode>
+
 );
